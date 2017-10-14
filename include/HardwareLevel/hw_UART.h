8e9392af8ffa9b7e128efcfe37078a09f8ffdc08
John Crannell-Ash <JAsh@Construction-Robotics.com>
2017-10-13 21:34:40 -0400

    Backup for writing using uprintf + other paramaters

--- include/HardwareLevel/hw_UART.h
+++ include/HardwareLevel/hw_UART.h
@@ -1,3 +1,7 @@
 #pragma once
 
 void Uart_init();
+
+
+int
+uprintf(UART_HandleTypeDef handle, const char* format, ...);
--- src/HardwareLevel/hw_UART.c
+++ src/HardwareLevel/hw_UART.c
@@ -1,16 +1,24 @@
 #include "stm32f4xx_hal.h"
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include "hw_Definitions.h"
 #include "hw_UART.h"
 #include "main.h" /* Let's change this to a hardware location */
 
+
+#ifndef OS_INTEGER_TRACE_PRINTF_TMP_ARRAY_SIZE
+#define OS_INTEGER_TRACE_PRINTF_TMP_ARRAY_SIZE (128)
+#endif
+
+  UART_HandleTypeDef huart;
 
 
 void Uart_init()
 {
   __USART2_CLK_ENABLE();
 
-  UART_HandleTypeDef huart;
+
   huart.Instance = USART2;
   huart.Init.BaudRate = 115200;
   huart.Init.WordLength = UART_WORDLENGTH_8B;
@@ -25,5 +33,33 @@ void Uart_init()
   }
 
 
-  HAL_UART_Transmit(&huart, (uint8_t*)"TEST BABY TEST", sizeof("TEST BABY TEST"), 0xFFFF);
+  HAL_UART_Transmit(&huart, (uint8_t*)"TEST BABY TEST\n\r", sizeof("TEST BABY TEST"), 0xFFFF);
+  uprintf(huart, "TEST 2 BABY\n\r");
+  uprintf(huart, "Test %d BABY\n\r", 3);
 }
+
+
+int
+uprintf(UART_HandleTypeDef  handle, const char* format, ...)
+{
+  int ret;
+  va_list ap;
+
+  va_start (ap, format);
+
+  // TODO: rewrite it to no longer use newlib, it is way too heavy
+
+  static char buf[OS_INTEGER_TRACE_PRINTF_TMP_ARRAY_SIZE];
+
+  // Print to the local buffer
+  ret = vsnprintf (buf, sizeof(buf), format, ap);
+  if (ret > 0)
+    {
+      // Transfer the buffer to the device
+      ret = HAL_UART_Transmit(&huart, (uint8_t*)buf, (size_t)ret, 0xFFFF);
+    }
+
+  va_end (ap);
+  return ret;
+}
+
